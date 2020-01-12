6e522c7f1f246025076d0074d8afcd9e03653247
DEMENTOR <dmntr@rambler.ru>
2020-01-12 21:22:42 +1000

    Socialite installation

--- public/js/app.js
+++ public/js/app.js
@@ -49506,8 +49506,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! d:\OpenServer\OSPanel\domains\what-is-in-my-fridge.loc\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! d:\OpenServer\OSPanel\domains\what-is-in-my-fridge.loc\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\OpenServer\OSPanel\domains\what-is-in-my-fridge.loc\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\OpenServer\OSPanel\domains\what-is-in-my-fridge.loc\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
